30b057ec27870418f61fadb724bb886e876c78c0
Ajit Singh <ajitsinghkaler0@gmail.com>
2025-02-01 18:21:13 +0530

    tailiwnd

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,9 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: ` <h1>{{ title }}</h1>`,
+  styles: `
+    h1 {
+      @apply text-4xl font-bold;
+    }`,
 })
 export class AppComponent {
   title = 'test';
